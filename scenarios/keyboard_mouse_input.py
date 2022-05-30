6a5eb510e7b43cb527392918e61c5d3cb8e29679
Berkantyuks <byuksektepe@hotmail.com>
2022-05-30 22:15:15 +0300

    oop optimized

--- scenarios/keyboard_mouse_input.py
+++ scenarios/keyboard_mouse_input.py
@@ -6,6 +6,6 @@ class keyboard_mouse_input_scenario:
     def start(self, driver):
         element_input = driver.find_element(By.ID, "name")
         element_input.click()
-        element_input.send_keys("Berkant")
+        element_input.send_keys("Berkant Yüksektepe")
         element_button = driver.find_element(By.ID, "button")
         element_button.click()
