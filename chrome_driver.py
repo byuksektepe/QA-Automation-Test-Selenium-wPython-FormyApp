55bc962e9da96386d02e7ec7f8eb647f50c19493
Berkantyuks <byuksektepe@hotmail.com>
2022-05-30 20:49:42 +0300

    click_element func updated

---
+++ chrome_driver.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+class chrome_driver:
+
+    def set(self):
+        chrome_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        return chrome_driver
+#end
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import time
-
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from chrome_driver import chrome_driver
 from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 class main:
-    chrome_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_driver = chrome_driver().set()
     chrome_driver.get('https://formy-project.herokuapp.com/')
 
     def Get_Formy_App():
@@ -29,7 +26,7 @@ class main:
         time.sleep(2)
         main.chrome_driver.quit()
 
-    def click_element(find_element_mode, item):
+    def click_element(find_element_mode, item, page_load_wait_id):
         s = 1
         if find_element_mode == "XPATH":
             click_element = main.chrome_driver.find_element(By.XPATH, item)
@@ -47,7 +44,7 @@ class main:
             click_element.click()
             timeout = 5
             try:
-                element_present = EC.presence_of_element_located((By.ID, 'name'))
+                element_present = EC.presence_of_element_located((By.ID, page_load_wait_id))
                 WebDriverWait(main.chrome_driver, timeout).until(element_present)
             except TimeoutException:
                 print("Timed out waiting for page to load")
@@ -56,10 +53,13 @@ class main:
 
 
 main.Get_Formy_App()
-main.click_element(find_element_mode="XPATH", item="/html/body/div/div/li[9]/a")
-input_tag = main.chrome_driver.find_element(By.ID, "name")
-input_tag.click()
-input_tag.send_keys("Berkant")
+main.click_element(find_element_mode="XPATH", item="/html/body/div/div/li[9]/a", page_load_wait_id="name")
+element_input = main.chrome_driver.find_element(By.ID, "name")
+element_input.click()
+element_input.send_keys("Berkant")
+
+element_button = main.chrome_driver.find_element(By.ID, "button")
+element_button.click()
 
 # Call This lAST!
 main.sleep_and_quit()
