69f80739adb424ba2ff04e7b832455243b9484d7
Berkantyuks <byuksektepe@hotmail.com>
2022-06-01 00:08:13 +0300

    full form test added comments

--- scenarios/full_form.py
+++ scenarios/full_form.py
@@ -10,14 +10,21 @@ class full_form:
         ID_FIELDS = [["first-name", "Berkant"], ["last-name", "Yüksektepe"],
                      ["job-title", "QA Test Automation Engineer"]]
 
+        # Türkçe devam edelim, ID_FIELD arrayine göre sırasıyla inputları doldur
         for ID_FIELD in ID_FIELDS:
             driver.find_element(By.ID, ID_FIELD[0]).send_keys(ID_FIELD[1])
 
+        # Sayfanın sonuna js ile kaydır
         driver.execute_script("window.scrollBy(0,250);")
+
+        # Radio ve checkboxları işaretle
         driver.find_element(By.CSS_SELECTOR, "input[value='radio-button-2']").click()
         driver.find_element(By.CSS_SELECTOR, "input[value='checkbox-1']").click()
 
+        # Select boxu seç
         driver.find_element(By.CSS_SELECTOR, "option[value='2']").click()
+
+        # Tarih gir
         driver.find_element(By.ID, "datepicker").send_keys("06/01/2022")
 
 
