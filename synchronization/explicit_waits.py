4893685047ed40675af249e2c485dfcffb208ddc
Berkantyuks <byuksektepe@hotmail.com>
2022-06-01 00:17:49 +0300

    oop optimization

--- synchronization/explicit_waits.py
+++ synchronization/explicit_waits.py
@@ -1,6 +1,6 @@
 import time
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 
