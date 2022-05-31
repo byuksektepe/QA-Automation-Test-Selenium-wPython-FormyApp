8643ab908ad31ee0bd3fb42ee56a873150b06b4f
Berkantyuks <byuksektepe@hotmail.com>
2022-05-31 19:49:27 +0300

    radio_buttons_and_checkboxes Updated

--- common_components/radio_buttons_and_checkboxes.py
+++ common_components/radio_buttons_and_checkboxes.py
@@ -3,3 +3,23 @@ from selenium.webdriver.common.by import By
 class radio_buttons_and_checkboxes:
     def start(self, driver):
         driver.get("https://formy-project.herokuapp.com/radiobutton")
+
+        el_radio_1 = driver.find_element(By.ID, "radio-button-1")
+        el_radio_1.click()
+
+        el_radio_2 = driver.find_element(By.CSS_SELECTOR, "input[value='option2']")
+        el_radio_2.click()
+
+        el_radio_3 = driver.find_element(By.CSS_SELECTOR, "input[value='option3']")
+        el_radio_3.click()
+
+        driver.get("https://formy-project.herokuapp.com/checkbox")
+
+        el_check_1 = driver.find_element(By.ID, "checkbox-1")
+        el_check_1.click()
+
+        el_check_2 = driver.find_element(By.ID, "checkbox-2")
+        el_check_2.click()
+
+        el_check_3 = driver.find_element(By.ID, "checkbox-3")
+        el_check_3.click()
