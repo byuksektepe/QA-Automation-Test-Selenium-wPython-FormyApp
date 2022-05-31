c8ea609985df9f632b2ab265637c8f8b6b27c924
Berkantyuks <byuksektepe@hotmail.com>
2022-05-31 21:36:15 +0300

    dropdown Updated

--- common_components/dropdown.py
+++ common_components/dropdown.py
@@ -4,4 +4,8 @@ from selenium.webdriver.common.by import By
 class dropdown:
     def start(self, driver):
         driver.get("https://formy-project.herokuapp.com/dropdown")
-        # el = driver.find_element(By.ID, "")
+        el_dropdown_menu = driver.find_element(By.ID, "dropdownMenuButton")
+        el_dropdown_menu.click()
+
+        item = driver.find_element(By.XPATH, "/html/body/div/div/div/a[8]")
+        item.click()
